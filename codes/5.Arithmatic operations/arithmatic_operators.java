a822fcae1fa6b22aec2c22ca5a922cf9aa9f8df8
techworldthink <jobin25051999@gmail.com>
2021-04-28 17:52:20 +0530

    arithmatic opr java

---
+++ codes/5.Arithmatic operations/arithmatic_operators.java
@@ -0,0 +1,9 @@
+public class arithmatic_operators{
+    public static void main(String []args){
+        System.out.println(1 + 2);
+        System.out.println(1 - 2);
+        System.out.println(1 * 2);
+        System.out.println(1 / 2);
+        System.out.println(1 % 2);
+    }
+}
