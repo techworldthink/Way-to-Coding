7a3a252c4c43398889ee44d4b4e8f1f336fd21c6
techworldthink <jobin25051999@gmail.com>
2021-04-28 18:00:38 +0530

    comments java py

---
+++ codes/6.Comments/comment.java
@@ -0,0 +1,12 @@
+public class comment {
+    public static void main(String[] args) {
+        // single line comment
+
+        /*
+         * multiple 
+         * line
+         * comment
+         */
+    }
+
+}
---
+++ codes/6.Comments/comments.py
@@ -0,0 +1,6 @@
+# single line comment
+
+""" multiple
+    line
+    comment
+"""
