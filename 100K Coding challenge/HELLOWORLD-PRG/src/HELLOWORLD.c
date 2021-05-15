b67e6b21dbfa256847d580fb4381c90cdcfbd17a
Viswajith J <viswajith.j.official@gmail.com>
2021-05-15 16:32:10 +0530

    made a hello world program

---
+++ 100K Coding challenge/HELLOWORLD-PRG/src/HELLOWORLD.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : BASICS-OF-C.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello \nI am new to learning C programming!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
