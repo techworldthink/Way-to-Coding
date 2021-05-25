5973e9fa16a677a6c9fbe21dc9e1fc48d3ad22bb
Viswajith J <viswajith.j.official@gmail.com>
2021-05-25 13:14:08 +0530

    Made a separate folder for Java and made some basic programs

---
+++ 100K Coding challenge/JAVA-100K/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String ar[]) {
+        String name = "Viswajith J";
+        System.out.println("Hello, World\nWelcome to Java");
+        System.out.println("I am "+name);
+    }
+}
