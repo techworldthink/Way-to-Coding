39f34462d93007498019956283d29d72869009ee
techworldthink <jobin25051999@gmail.com>
2021-06-28 19:50:49 +0530

    python mul table

---
+++ MCA Exam Preparations/Python/SNGIST/3 multiplication table.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    number = int(input("Enter a number : "))
+    for i in range(1,11):
+        print(i," x ",number," = ",i*number)
+    
+while(True):    
+    main()
---
+++ MCA Exam Preparations/Python/SNGIST/4.check positive or not.py
@@ -0,0 +1,8 @@
+
+def main():
+    number = int(input("Enter a number : "))
+    if(number>0):print("positive")
+    else:print("negative")
+    
+while(True):    
+    main()
