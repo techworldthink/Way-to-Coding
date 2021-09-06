2971f9456aec4c04dae4793de3da38f60cc2cd47
techworldthink <jobin25051999@gmail.com>
2021-09-06 14:37:08 +0530

    JAVA - foreachloop

---
+++ JAVA/9_For-Each Loop.java
@@ -0,0 +1,8 @@
+class ForEachLoop {
+    public static void main(String[] args) {
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
