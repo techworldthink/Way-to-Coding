6a5f937451ec07d6a3debc7d5aff224cb5d9ea47
techworldthink <jobin25051999@gmail.com>
2021-09-06 14:25:45 +0530

    JAVA - math

---
+++ JAVA/6_math.java
@@ -0,0 +1,9 @@
+class math {
+    public static void main(String[] args) {
+        System.out.println(Math.min(1, 2));
+        System.out.println(Math.max(1, 2));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.abs(-12));
+        System.out.println(Math.random());
+    }
+}
