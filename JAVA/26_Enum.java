db9eb50f01992519bc51a81f3566c347d7ec6b9e
techworldthink <jobin25051999@gmail.com>
2021-09-07 12:23:50 +0530

    JAVA - OOPS enum

---
+++ JAVA/26_Enum.java
@@ -0,0 +1,10 @@
+class Main {
+    enum Level {
+        LOW, MEDIUM, HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
