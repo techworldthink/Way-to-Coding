d5a6c0ee99fe2d8235235d7df9d9fb3f91da5d51
techworldthink <jobin25051999@gmail.com>
2021-09-07 12:50:32 +0530

    JAVA - user input

---
+++ JAVA/28_user_input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name : ");
+        String name  = sc.nextLine();
+        System.out.println("Name : "+name);
+        // nextInt()
+    }
+}
