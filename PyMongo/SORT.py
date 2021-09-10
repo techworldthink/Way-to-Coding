14aeb3f0d533be9d7adf193ff05595210b5eb61e
techworldthink <jobin25051999@gmail.com>
2021-09-10 19:22:26 +0530

    pymongo sort

--- PyMongo/SORT.py
+++ PyMongo/SORT.py
@@ -7,18 +7,15 @@ myDb = client["TESTING"]
 myCollection = myDb["DATA"]
 
 
-# filter
+# Sort
 
-print("\nDocument with _id = 1\n")
-for data in myCollection.find({"_id":1}):
+#ascending
+for data in myCollection.find().sort("type",1):
+    print(data)
+#descending
+for data in myCollection.find().sort("type",-1):
     print(data)
 
-print("\nDocument with id > 1\n")
-for data in myCollection.find({"id":{"$gte":1}}):
-    print(data)
 
-print("\nDocument with data field value starts with ^o")
-for data in myCollection.find({"data":{"$regex":"^o"}}):
-    print(data)
 
 
