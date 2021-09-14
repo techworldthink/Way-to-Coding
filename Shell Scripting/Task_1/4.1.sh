755e2023981f83b8de56181a397b36b712ab353d
techworldthink <jobin25051999@gmail.com>
2021-09-14 11:56:26 +0530

    task 4

---
+++ Shell Scripting/Task_1/4.1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# To create file
+# cat > myFile;
+printf "\nprint data on myFile use tabs to seperate\n";
+cat myFile | column -t -s ' ';
+
+printf "\nSwap 2,3 fields of myFile\n";
+cut -d " " -f 1,3 myFile  > cat  && cut -d " " -f 2 myFile | paste cat -;
+
+printf "\nprint first 2 row only\n";
+head -n  2  myFile;
+
+printf "\nPrint last row\n";
+head -n 3 myFile | tail -n 1;
+
+printf "\nPrint only names\n";
+cut -d " " -f 2 myFile;
+
+printf "\nPrint word count,  lines count , letter count  \n";
+cut -d " " -f 2  myFile | wc;
+
+#swap
+#file="myFile"
+#while read line;
+#do
+#echo $line;
+#cut -d " " -f 1  $line;
+#done < $file
+
+#print first 2 row 
+#awk '{ print $1 " " $2}' myFile;
