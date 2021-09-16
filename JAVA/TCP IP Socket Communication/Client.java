e82a9c027ef1919930024103e2c56fc5a7ea51f3
techworldthink <jobin25051999@gmail.com>
2021-09-16 14:29:58 +0530

    TCP IP

---
+++ JAVA/TCP IP Socket Communication/Client.java
@@ -0,0 +1,70 @@
+// A Java program for a Client
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+	// initialize socket and input output streams
+	private Socket socket		 = null;
+	private DataInputStream input = null;
+	private DataOutputStream out	 = null;
+
+	// constructor to put ip address and port
+	public Client(String address, int port)
+	{
+		// establish a connection
+		try
+		{
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+
+			// takes input from terminal
+			input = new DataInputStream(System.in);
+
+			// sends output to the socket
+			out = new DataOutputStream(socket.getOutputStream());
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+
+		// string to read message from input
+		String line = "";
+
+		// keep reading until "Over" is input
+		while (!line.equals("Over"))
+		{
+			try
+			{
+				line = input.readLine();
+				out.writeUTF(line);
+			}
+			catch(IOException i)
+			{
+				System.out.println(i);
+			}
+		}
+
+		// close the connection
+		try
+		{
+			input.close();
+			out.close();
+			socket.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		Client client = new Client("127.0.0.1", 5000);
+	}
+}
