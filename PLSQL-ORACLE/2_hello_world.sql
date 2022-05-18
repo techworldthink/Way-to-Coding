735be1e324700954e52aa55d6600d0dea3ac24ce
techworldthink <jobin25051999@gmail.com>
2022-05-18 22:12:03 +0530

    oracle - pl sql 1

---
+++ PLSQL-ORACLE/2_hello_world.sql
@@ -0,0 +1,5 @@
+DECLARE
+
+BEGIN
+    dbms_output.put_line('hello world');
+END;
---
+++ PLSQL-ORACLE/4_comments.sql
@@ -0,0 +1,8 @@
+DECLARE
+    -- comments 
+BEGIN
+   /*
+   multiline comments
+   */
+   dbms_output.put_line('executed...');
+END;
