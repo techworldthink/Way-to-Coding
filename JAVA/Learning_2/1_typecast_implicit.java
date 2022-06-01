9c0a6692f0ad8e6c65d03cf0d3d83fb1e00f3f93
techworldthink <jobin25051999@gmail.com>
2022-06-01 11:43:10 +0530

    JAVA typecasting

---
+++ JAVA/Learning_2/1_typecast_implicit.java
@@ -0,0 +1,12 @@
+class typecast_implicit {
+    public static void main(String []args){
+        byte b = 100;
+        int i = b;
+        System.out.println(i);
+        long l = i;
+
+        char ch = 'A';
+        int x = ch;
+        System.out.println(x);
+    }
+}
