659594a3939424d85ee8df263f22a89e39f83773
techworldthink <jobin25051999@gmail.com>
2022-06-11 22:09:24 +0530

    JAVA Learning 3.1

---
+++ JAVA/Learning_3/1_String_tokenizer.java
@@ -0,0 +1,9 @@
+import java.util.StringTokenizer;  
+class Simple{  
+ public static void main(String args[]){  
+   StringTokenizer st = new StringTokenizer("my name is khan"," ");  
+     while (st.hasMoreTokens()) {  
+         System.out.println(st.nextToken());  
+     }  
+   }  
+} 
---
+++ JAVA/Learning_3/3_calendar.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class CalendarExample {
+    public static void main(String args[])
+    {
+        Calendar c = Calendar.getInstance();
+        System.out.println("The Current Date is:" + c.getTime());
+    }
+}
