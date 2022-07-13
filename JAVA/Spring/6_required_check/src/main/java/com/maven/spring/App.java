a33de6b0df8224cd6f1742e4d6dbfd67b539cfab
techworldthink <jobin25051999@gmail.com>
2022-07-13 20:56:38 +0530

    JAVA SPRING - required annotation

---
+++ JAVA/Spring/6_required_check/src/main/java/com/maven/spring/App.java
@@ -0,0 +1,11 @@
+package com.maven.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+
+}
