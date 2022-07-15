35ec668aea68fb2ee152f529d05f01b26eaa4985
techworldthink <jobin25051999@gmail.com>
2022-07-15 09:00:06 +0530

    JAVA SPRING autowire annotations

---
+++ JAVA/Spring/20_autowire_annotation/src/main/java/com/maven/spring/autowiring/annotations/Employee.java
@@ -0,0 +1,27 @@
+package com.maven.spring.autowiring.annotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+
+	private Address address;
+	
+	public Employee(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+	@Autowired
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
