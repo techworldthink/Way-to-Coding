08c968bdfb217004844dc888ebced9c2501c1531
techworldthink <jobin25051999@gmail.com>
2022-07-15 07:35:16 +0530

    JAVA SPRING autowiring

---
+++ JAVA/Spring/17_autowiring/src/main/java/com/maven/spring/autowiring/Employee.java
@@ -0,0 +1,20 @@
+package com.maven.spring.autowiring;
+
+public class Employee {
+
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
