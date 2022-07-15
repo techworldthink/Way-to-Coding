2a6dcf94ec8d480d9ec264892b7854a9497bf802
techworldthink <jobin25051999@gmail.com>
2022-07-15 08:43:09 +0530

    JAVA SPRING autowiring by name and constructor

---
+++ JAVA/Spring/19_autowiring_constructor/src/main/java/com/maven/spring/autowiring/Employee.java
@@ -0,0 +1,24 @@
+package com.maven.spring.autowiring;
+
+public class Employee {
+
+	private Address address;
+	
+	public Employee(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
