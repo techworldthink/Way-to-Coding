4b8db70d29f08d9af203d91a7156858efa5d3f89
techworldthink <jobin25051999@gmail.com>
2022-07-18 23:09:36 +0530

    JAVA SPRING UNIT TEST getting started with junit

---
+++ JAVA/Unit Testing/1_Getting started with junit/src/test/java/com/spring/learn/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.spring.learn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
