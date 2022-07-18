fc492774ae6e375666f4f76363c3ea53912c1db6
techworldthink <jobin25051999@gmail.com>
2022-07-18 14:33:49 +0530

    JAVA SPRING JDBC insert 2

---
+++ JAVA/Spring/37_JDBC_Insert_dao/src/main/java/com/maven/spring/jdbc/learnJdbc/employee/dao/EmployeeDao.java
@@ -0,0 +1,7 @@
+package com.maven.spring.jdbc.learnJdbc.employee.dao;
+
+import com.maven.spring.jdbc.learnJdbc.employee.dto.Employee;
+
+public interface EmployeeDao {
+	int create(Employee employee);
+}
