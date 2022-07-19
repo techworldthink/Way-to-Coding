2039d90ed35339e8d01a49ab9036f0eac106cc17
techworldthink <jobin25051999@gmail.com>
2022-07-19 17:36:57 +0530

    JAVA SPRING UNIT TEST arrays compare

---
+++ JAVA/Unit Testing/7_ArrayCompare/src/test/java/com/spring/learn/ArraysCompareTest.java
@@ -0,0 +1,19 @@
+package com.spring.learn;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ArraysCompareTest {
+
+	@Test
+	public void testArraySort_RandomArray() {
+		int[] numbers = { 12, 3, 4, 1 };
+		int[] expected = { 1, 3, 4, 12 };
+		Arrays.sort(numbers);
+		assertArrayEquals(expected, numbers);
+	}
+
+}
