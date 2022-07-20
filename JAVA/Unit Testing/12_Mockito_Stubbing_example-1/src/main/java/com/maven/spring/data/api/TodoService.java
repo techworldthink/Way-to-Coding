50233d999262586c55b8d07f2f2e1e230f03db80
techworldthink <jobin25051999@gmail.com>
2022-07-20 09:56:46 +0530

    JAVA SPRING UNIT TEST mockito stubbing

---
+++ JAVA/Unit Testing/12_Mockito_Stubbing_example-1/src/main/java/com/maven/spring/data/api/TodoService.java
@@ -0,0 +1,9 @@
+package com.maven.spring.data.api;
+
+import java.util.List;
+
+// Create TodoServiceStub
+// Test TodoBusinessImpl using TodoService
+public interface TodoService {
+	public List<String> retrieveTodos(String user);
+}
