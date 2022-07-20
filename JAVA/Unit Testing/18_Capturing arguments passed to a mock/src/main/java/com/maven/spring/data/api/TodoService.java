f36691425d9864ebb56d2b0295163dc61b0499cc
techworldthink <jobin25051999@gmail.com>
2022-07-20 20:28:56 +0530

    JAVA SPRING UNIT TEST MOCKITO argument capturing

---
+++ JAVA/Unit Testing/18_Capturing arguments passed to a mock/src/main/java/com/maven/spring/data/api/TodoService.java
@@ -0,0 +1,10 @@
+package com.maven.spring.data.api;
+
+import java.util.List;
+
+// Create TodoServiceStub
+// Test TodoBusinessImpl using TodoService
+public interface TodoService {
+	public List<String> retrieveTodos(String user);
+	public void deleteTodo(String todo);
+}
