2e6a9be0cf74049051c902be80f6aacb0768de75
techworldthink <jobin25051999@gmail.com>
2022-07-21 22:57:57 +0530

    JAVA SPRING BOOT simple web app

---
+++ JAVA/Spring Boot MVC/1_Simple_webapp/src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,50 @@
+package webapp;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Browser sends Http Request to Web Server
+ * 
+ * Code in Web Server => Input:HttpRequest, Output: HttpResponse
+ * JEE with Servlets
+ * 
+ * Web Server responds with Http Response
+ */
+
+//Java Platform, Enterprise Edition (Java EE) JEE6
+
+//Servlet is a Java programming language class 
+//used to extend the capabilities of servers 
+//that host applications accessed by means of 
+//a request-response programming model.
+
+//1. extends javax.servlet.http.HttpServlet
+//2. @WebServlet(urlPatterns = "/login.do")
+//3. doGet(HttpServletRequest request, HttpServletResponse response)
+//4. How is the response created?
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Yahoo!!!!!!!!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("My First Servlet");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+}
---
+++ JAVA/Spring Boot MVC/2_Simple_jsp/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>login</title>
+</head>
+<body>
+Login page
+</body>
+</html>
