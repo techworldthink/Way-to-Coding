181824d5c173df20e25d7ee7e3e85500bb393fa0
techworldthink <jobin25051999@gmail.com>
2022-07-24 15:11:14 +0530

    SPRING BOOT add todo second appraoch

---
+++ JAVA/Spring Boot MVC/8_Simple Todo App (ADD) second approach/src/main/resources/META-INF/resources/WEB-INF/jsp/home.jsp
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Welcome Page</title>
+	</head>
+	<body>
+		<div class="container">
+			<h1>Welcome ${name}</h1>
+			<a href="list-todos">Manage</a> your todos
+		</div>
+	</body>
+</html>
---
+++ JAVA/Spring Boot MVC/8_Simple Todo App (ADD) second approach/src/test/java/com/spring/boot/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
