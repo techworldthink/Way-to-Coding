63f83d8b63fce506663050654e3984f8c23f3412
techworldthink <jobin25051999@gmail.com>
2022-08-05 07:35:30 +0530

    Angular form

--- Angular/13_Angular_Form/src/app/app.component.ts
+++ Angular/13_Angular_Form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp';
+
+  userData:any = {};
+  getData(data: NgForm) {
+    console.warn(data);
+    this.userData = data;
+    console.warn(this.userData.user);
+  }
+
 }
