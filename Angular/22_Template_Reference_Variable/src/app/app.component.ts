2197e9d91d69b90fc0d88591b1e1cd70ef8b1a02
techworldthink <jobin25051999@gmail.com>
2022-08-06 06:53:25 +0530

    Angular template reference variable

---
+++ Angular/22_Template_Reference_Variable/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularApp';
+
+  getVal(item: HTMLInputElement) {
+    console.warn(item);
+  }
+
+
+}
