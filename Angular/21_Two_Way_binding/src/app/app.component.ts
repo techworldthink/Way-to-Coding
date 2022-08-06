830e529c38629587b8fdad1c49d6e85947c17332
techworldthink <jobin25051999@gmail.com>
2022-08-06 06:40:14 +0530

    Angular two way binding

--- Angular/21_Two_Way_binding/src/app/app.component.ts
+++ Angular/21_Two_Way_binding/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp';
+
+  data:any;
+  
 }
--- Angular/21_Two_Way_binding/src/app/app.module.ts
+++ Angular/21_Two_Way_binding/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
