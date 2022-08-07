4f96f48778161ede0103458557807752c36d853e
techworldthink <jobin25051999@gmail.com>
2022-08-07 12:40:45 +0530

    MicroService JAVA limits service v2

---
+++ Microservice/JAVA/1_Limits-service_V2_values read from properties file/src/test/java/com/learn/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
