94ebac3229923092536517e3002dd1e139eca2c4
techworldthink <jobin25051999@gmail.com>
2022-08-12 19:53:45 +0530

    Microservice currency exchange v4 connected to eureka load balance

--- Microservice/JAVA/Currency Conversion and Exchange Service/develop/currency-conversion-service - v4 - conneted to eureka/currency-conversion-service/src/main/java/com/learn/ms/currencyconversionservice/CurrencyExchangeProxy.java
+++ Microservice/JAVA/Currency Conversion and Exchange Service/develop/currency-conversion-service - v4 - conneted to eureka/currency-conversion-service/src/main/java/com/learn/ms/currencyconversionservice/CurrencyExchangeProxy.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+// @FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
