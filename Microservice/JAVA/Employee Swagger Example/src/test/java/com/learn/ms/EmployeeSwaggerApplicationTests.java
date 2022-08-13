92c94dc938d9fd6760f0394923956bf16b66b3f0
techworldthink <jobin25051999@gmail.com>
2022-08-14 00:17:12 +0530

    Microservice employee swagger

---
+++ Microservice/JAVA/Employee Swagger Example/src/test/java/com/learn/ms/EmployeeSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
