60b10e6b03d4b4a893c278defca8a8d718c5fed6
techworldthink <jobin25051999@gmail.com>
2022-08-14 14:57:40 +0530

    Microservice sqs producer

---
+++ Cloud/AWS SQS/sqs-producer/src/test/java/com/ms/sqsproducer/SqsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.sqsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
