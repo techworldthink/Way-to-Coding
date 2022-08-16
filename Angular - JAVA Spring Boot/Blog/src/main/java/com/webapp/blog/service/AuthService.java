630a013342ae6ee45a43b2a0cc18bce8fab61be2
techworldthink <jobin25051999@gmail.com>
2022-08-16 09:25:12 +0530

    Blog spring server  - password encryption

--- Angular - JAVA Spring Boot/Blog/src/main/java/com/webapp/blog/service/AuthService.java
+++ Angular - JAVA Spring Boot/Blog/src/main/java/com/webapp/blog/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.webapp.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.webapp.blog.dto.RegisterRequest;
@@ -11,14 +12,20 @@ import com.webapp.blog.repository.UserRepository;
 public class AuthService {
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
 		user.setUserName(registerRequest.getUsername());
 		user.setEmail(registerRequest.getEmail());
-		user.setPassword(registerRequest.getPassword());
+		user.setPassword(encodePassword(registerRequest.getPassword()));
 
 		userRepository.save(user);
+	}
+
+	private String encodePassword(String password) {
+		return passwordEncoder.encode(password);
 	}
 
 }
