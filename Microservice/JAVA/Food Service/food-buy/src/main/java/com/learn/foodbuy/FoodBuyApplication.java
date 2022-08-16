38bdf9f322f4117e3baecad2ea91489d817eda0d
techworldthink <jobin25051999@gmail.com>
2022-08-16 18:34:39 +0530

    Microservice food service initial

---
+++ Microservice/JAVA/Food Service/food-buy/src/main/java/com/learn/foodbuy/FoodBuyApplication.java
@@ -0,0 +1,13 @@
+package com.learn.foodbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBuyApplication.class, args);
+	}
+
+}
