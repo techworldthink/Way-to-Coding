cb650d5d4c75c5f3bb976d098a76e850b98bf87f
techworldthink <jobin25051999@gmail.com>
2022-08-17 09:55:18 +0530

    Microservice food buy done

--- Microservice/JAVA/Food Service(Microservice communicate - OpenFeign)/food-buy/src/main/java/com/learn/foodbuy/FoodBuyApplication.java
+++ Microservice/JAVA/Food Service(Microservice communicate - OpenFeign)/food-buy/src/main/java/com/learn/foodbuy/FoodBuyApplication.java
@@ -2,8 +2,10 @@ package com.learn.foodbuy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FoodBuyApplication {
 
 	public static void main(String[] args) {
