1c3da79b8c40429fa24f778828ec87d46deca5c9
techworldthink <jobin25051999@gmail.com>
2022-08-19 20:08:53 +0530

    Microservice Associate-Manager initial

---
+++ Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/ManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerApplication.class, args);
+	}
+
+}
