16f8743688ff054268b6e6e56a3ff17b17b0ce83
techworldthink <jobin25051999@gmail.com>
2022-08-21 23:19:19 +0530

    Microservice Associate-Manager openfeign client added

---
+++ Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/client/AssociatesClient.java
@@ -0,0 +1,14 @@
+package com.project.manager.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.project.manager.entity.Associates;
+import com.project.manager.exception.AssociateNotFoundException;
+
+@FeignClient(url = "$associate.url}", name = "food-service")
+public interface AssociatesClient {
+
+	@GetMapping(value = "/getassociate/{id}")
+	public Associates getFoodById(@PathVariable("id") int id) throws AssociateNotFoundException;
+}
