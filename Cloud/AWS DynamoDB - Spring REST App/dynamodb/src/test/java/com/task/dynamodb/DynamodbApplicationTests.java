da380e3afd183a67dabcce0ff944adc49122af52
techworldthink <jobin25051999@gmail.com>
2022-08-23 08:34:39 +0530

    Cloud dynamodb with spring

---
+++ Cloud/AWS DynamoDB - Spring REST App/dynamodb/src/test/java/com/task/dynamodb/DynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
