a80dd347ab68b895a8c2528d6c7e7aa6968bc84d
techworldthink <jobin25051999@gmail.com>
2022-08-29 19:35:29 +0530

    MS HM_Associates - auth app added

---
+++ Microservice/JAVA/Associates - Manager/AuthorizationRole/AuthorizationRole/AuthorizationRole/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (userid,upassword,uname,auth_role) values ('admin','admin','admin','admin');
+insert into user (userid,upassword,uname, auth_role) values ('foo','foo','foo','associate');
+insert into user (userid,upassword,uname, auth_role) values ('abcd','abcd','abcd','homemanager');
