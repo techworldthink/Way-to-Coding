699c62611450bbb7412915ca50eb24aaab664ccc
techworldthink <jobin25051999@gmail.com>
2022-09-02 00:44:26 +0530

    MS HM_Associates - integration stage 1

--- Microservice/JAVA/Associates - Manager/authentication/src/main/java/com/project/authorization/AuthorizationMicroserviceApplication.java
+++ Microservice/JAVA/Associates - Manager/authentication/src/main/java/com/project/authorization/AuthorizationMicroserviceApplication.java
@@ -3,6 +3,9 @@ package com.project.authorization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
--- Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/ManagerApplication.java
+++ Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/ManagerApplication.java
@@ -2,10 +2,12 @@ package com.project.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class ManagerApplication {
 
 	public static void main(String[] args) {
