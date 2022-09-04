60c6fc87012118239b1639a0b2d1e04499918266
techworldthink <jobin25051999@gmail.com>
2022-09-04 07:25:51 +0530

    MS HM_Associates - manager validation stage 3

--- Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/service/ManagerService.java
+++ Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/service/ManagerService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.project.manager.client.UserDetailsClient;
 import com.project.manager.entity.Employee;
 import com.project.manager.exception.EmployeeEmptyException;
+import com.project.manager.exception.EmployeeNotFoundException;
 import com.project.manager.exception.ManagerNotFoundException;
 import com.project.manager.validator.ManagerValidator;
 
@@ -28,19 +29,26 @@ public class ManagerService {
 		managerValidator.validateEmail(homeManager.getEmail());
 	}
 
+	private void isHomeManager(int id, String token) throws ManagerNotFoundException, EmployeeNotFoundException {
+		Employee manager = userDetailsClient.getEmployeeById(id, token);
+		if (!manager.isHomeManager())
+			throw new ManagerNotFoundException("Manager not found");
+	}
+
+	
+	
 	public List<Employee> getAllManagers(String token) throws Exception {
 		return userDetailsClient.getHomeManagers(token);
 	}
 
 	public Employee getManagerById(int id, String token) throws Exception {
+		isHomeManager(id, token);
 		Employee manager = userDetailsClient.getEmployeeById(id, token);
-		if (manager.isHomeManager())
-			return manager;
-		else
-			throw new ManagerNotFoundException("Manager not found");
+		return manager;
 	}
 
-	public Employee deleteManagerById(int id, String token) throws ManagerNotFoundException {
+	public Employee deleteManagerById(int id, String token) throws ManagerNotFoundException, EmployeeNotFoundException {
+		isHomeManager(id, token);
 		return userDetailsClient.deleteEmployee(id, true, token);
 	}
 
@@ -51,7 +59,9 @@ public class ManagerService {
 	}
 
 	public Employee updateManager(int id, Employee homeManager, String token) throws Exception {
+		isHomeManager(id, token);
 		validateHomeManager(homeManager);
 		return userDetailsClient.updateEmployee(id, homeManager, token);
+
 	}
 }
