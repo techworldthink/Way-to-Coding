b7005f9235bea8af3855b33e6af0c53bb073558a
techworldthink <jobin25051999@gmail.com>
2022-09-05 18:57:27 +0530

    MS HM_Associates - manager repo annotation problem fixed

--- Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/repository/AssignedRepository.java
+++ Microservice/JAVA/Associates - Manager/home-manager/src/main/java/com/project/manager/repository/AssignedRepository.java
@@ -1,11 +1,10 @@
 package com.project.manager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
+import org.springframework.stereotype.Repository;
 import com.project.manager.entity.AssignManagers;
 
-@Service
+@Repository
 public interface AssignedRepository extends JpaRepository<AssignManagers, Integer> {
 
 	boolean existsByEmployeeId(int associateId);
