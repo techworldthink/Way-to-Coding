813346766dfc5cf45dd089dae3d8d6ce3bd0d3c0
techworldthink <jobin25051999@gmail.com>
2022-09-18 13:41:56 +0530

    associates spring app - updated

--- Microservice/JAVA/Associates - Manager/authentication/src/main/resources/data.sql
+++ Microservice/JAVA/Associates - Manager/authentication/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into user (userid,upassword,uname,auth_role) values ('admin','admin','admin','admin');
+insert into user (userid,upassword,uname,auth_role) values ('manager','manager','manager','manager');
 insert into user (userid,upassword,uname, auth_role) values ('employee','employee','employee','employee');
 insert into user (userid,upassword,uname, auth_role) values ('guest','guest','guest','guest');
