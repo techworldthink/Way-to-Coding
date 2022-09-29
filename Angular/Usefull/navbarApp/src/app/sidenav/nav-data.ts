d7959942b7ff308c9a9a5c925aa37f2998e1551d
techworldthink <jobin25051999@gmail.com>
2022-09-29 12:36:07 +0530

    Angular navbar design added

---
+++ Angular/Usefull/navbarApp/src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'fal fa-home',
+        label: 'Dashboard'
+    },
+    {
+        routeLink: 'products',
+        icon: 'fal fa-box-open',
+        label: 'Products'
+    },
+    {
+        routeLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Statistics'
+    },
+    {
+        routeLink: 'coupens',
+        icon: 'fal fa-tags',
+        label: 'Coupens'
+    },
+    {
+        routeLink: 'pages',
+        icon: 'fal fa-file',
+        label: 'Pages'
+    },
+    {
+        routeLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Media'
+    },
+    {
+        routeLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Settings'
+    },
+];
