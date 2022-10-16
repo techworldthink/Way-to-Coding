ee8ba9a0a2e10a2ff7d4a2e618e7cc294994f316
techworldthink <jobin25051999@gmail.com>
2022-10-16 15:49:52 +0530

    ESP32 - WIFI AP&STA MODE

---
+++ ESP32/Wi-Fi/WIFI_MODE_APSTA/WIFI_MODE_APSTA.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+
+const char* wifi_network_ssid = "DEN;BB JOBIN";
+const char* wifi_network_password =  "jobin@1421";
+
+const char *soft_ap_ssid = "MyESP32AP";
+const char *soft_ap_password = "testpassword";
+
+void setup() {
+
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_MODE_APSTA);
+
+  WiFi.softAP(soft_ap_ssid, soft_ap_password);
+  WiFi.begin(wifi_network_ssid, wifi_network_password);
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.print("ESP32 IP as soft AP: ");
+  Serial.println(WiFi.softAPIP());
+
+  Serial.print("ESP32 IP on the WiFi network: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {}
