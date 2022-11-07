a05faa93b626385fb6dba6d0e640e52d81013376
techworldthink <jobin25051999@gmail.com>
2022-11-07 13:04:20 +0530

    ESP32 - capital portal - wifi connected and redirect to browser

---
+++ Data Structure/JAVA/3_LinkedList/Doubly linked lists/Node.java
@@ -0,0 +1,11 @@
+public class Node<T extends Comparable<T>>{
+    private T data;
+    private Node<T> previousNode;
+    private Node<T> nexNode;
+
+    public Node(T data){
+        this.data = data;
+    }
+
+    public void setData
+}
