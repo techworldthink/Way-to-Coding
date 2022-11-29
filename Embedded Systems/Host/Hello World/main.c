8dbedf1908a4fb103fa1f936ed0a5ea5e1bc289b
techworldthink <jobin25051999@gmail.com>
2022-11-29 13:50:25 +0530

    Embedded - Host - Hello world

---
+++ Embedded Systems/Host/Hello World/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 29-Nov-2022
+ *      Author: Jobin
+ */
+
+
+#include<stdio.h>
+
+int main(void){
+	printf("Hello world\n");
+	printf("Press Enter to exit this application\n");
+	getchar();
+	return 0;
+}
