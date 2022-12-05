86e8425900fd9debd1ab2d896ecbdd02e9b1ca83
techworldthink <jobin25051999@gmail.com>
2022-12-05 23:59:13 +0530

    JAVA - SQL quries project started

---
+++ JAVA/SQL/SpringBoot/sql/src/main/java/com/learn/sql/SqlApplication.java
@@ -0,0 +1,13 @@
+package com.learn.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlApplication.class, args);
+	}
+
+}
