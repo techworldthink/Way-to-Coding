a2cf43f06f4de99587e8a1dad6bc5faa2cedac48
Apz-26 <aparnakulangara@gmail.com>
2023-03-31 16:53:15 +0530

    Initial commit

---
+++ Python/Assignments/Write a Python program to sort a list of integers in ascending and descending order.py
@@ -0,0 +1,7 @@
+a=[5,10,12,16,4,3,9,21,25]
+#To arrange the list in ascending order
+a.sort()
+print("Ascending order= ",a)
+#To arrange the list in descending order
+a.sort(reverse = True)
+print("Descenting order=",a)
