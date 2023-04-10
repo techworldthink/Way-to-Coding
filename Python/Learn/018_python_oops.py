61aef84cc110fde7086fbf626fa6e3558ae90508
techworldthink <jobin25051999@gmail.com>
2023-04-10 23:39:22 +0530

    python oops

--- Python/Learn/018_python_oops.py
+++ Python/Learn/018_python_oops.py
@@ -8,10 +8,11 @@ class TestClass:
     # constructor
     def __init__(self,num):
         self.num = num
+        self.num_2 = num
         print("This is a constructor") 
     
     def __str__(self):
-        return f"{self.data} - {self.num}"
+        return f"{self.data} - {self.num} - {self.num_2}"
 
     # object method
     # self - this parameter is a reference to the current instance.
@@ -31,6 +32,15 @@ print(obj.get_data()) # 123
 # modify object property
 obj.data = 89
 print(obj.data) # 89
+
+# its a class attribute, so it will set default value
+del obj.data
+print(obj)
+
+print(obj.num_2)
+del obj.num_2
+# AttributeError: 'TestClass' object has no attribute 'num_2'
+# print(obj.num_2)
 
 # delete object
 del obj
