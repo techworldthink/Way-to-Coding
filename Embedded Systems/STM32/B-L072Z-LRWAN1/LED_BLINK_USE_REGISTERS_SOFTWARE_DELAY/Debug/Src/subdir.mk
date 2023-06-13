6542db4e3e76067aa74afa5f9f32ffa1c0e31773
techworldthink <jobin25051999@gmail.com>
2023-06-13 12:01:48 +0530

    STM LED blink use Registers bitwise operation and software delay

---
+++ Embedded Systems/STM32/B-L072Z-LRWAN1/LED_BLINK_USE_REGISTERS_SOFTWARE_DELAY/Debug/Src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DB_L072Z_LRWAN1 -DSTM32 -DSTM32L072CZYx -DSTM32L0 -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+
+.PHONY: clean-Src
+
---
+++ Embedded Systems/STM32/B-L072Z-LRWAN1/LED_BLINK_USE_REGISTERS_SOFTWARE_DELAY/Debug/Startup/startup_stm32l072czyx.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32l072czyx.o: ../Startup/startup_stm32l072czyx.s
