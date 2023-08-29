a20eaa4a94fdfbc1063b654fd89ef69f6b686458
techworldthink <jobin25051999@gmail.com>
2023-08-29 22:40:13 +0530

    esp32 http request

---
+++ Embedded Systems/ESP32/ESP32-DEV-KIT/HTTP_Request_Send/HTTP_Request_Send.ino
@@ -0,0 +1,56 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "wi-fi ssid";
+const char* password = "wi-fi pass";
+
+String url = "http://ifconfig.me/ip";
+
+
+void setup() {
+  Serial.begin(115200); 
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(url.c_str());
+      
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    delay(5000);
+}
