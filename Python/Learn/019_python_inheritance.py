3857068f48ae54bf1f179a68a3c8c0b478d4fa04
techworldthink <jobin25051999@gmail.com>
2023-11-11 16:05:21 +0530

    python learn 20

--- Python/Learn/019_python_inheritance.py
+++ Python/Learn/019_python_inheritance.py
@@ -1,26 +1,28 @@
-# parent class
+# Parent class
 class Author():
-    author_name=""
+    author_name = ""  # Class attribute to store author's name
 
-    def __init__(self,name):
-        self.author_name = name
+    def __init__(self, name):
+        self.author_name = name  # Constructor initializes the author_name attribute
 
     def __str__(self):
-        return f"{self.author_name}"
+        return f"{self.author_name}"  # String representation of the Author object
 
-# child class
+# Child class
 class Book(Author):
-    book_name = ""
+    book_name = ""  # Class attribute to store book's name
 
-    def __init__(self,bname,aname):
-        #Author.__init__(self,aname)
+    def __init__(self, bname, aname):
+        # Initialize the Book object, calling the parent class's constructor
         super().__init__(aname)
         self.book_name = bname
-    
+
     def __str__(self):
-        return f"{self.author_name} - {self.book_name}"
+        return f"{self.author_name} - {self.book_name}"  # String representation of the Book object
 
-# create object
-obj = Book("sherlock","holmes")
-print(obj)
+# Create an object
+obj = Book("sherlock", "holmes")
+print(obj)  # Print the string representation of the Book object
 
+# Output:
+# holmes - sherlock
--- Python/Learn/020_python_constructor_overrides.py
+++ Python/Learn/020_python_constructor_overrides.py
@@ -1,36 +1,48 @@
-
+# Inheritance without overriding the parent's __init__()
 class Parent1():
     def __init__(self):
         print("Parent constructor invoked")
+
 class Child1(Parent1):
     pass
+
 obj1 = Child1()
+# Output: "Parent constructor invoked"
+# Explanation: In this case, the child class (Child1) does not define its own __init__() method,
+# so it inherits the __init__() method from the parent class (Parent1).
 
-
-"""
-When you define an __init__() function in the child class, 
-it will override the parent's __init__() function and 
-the child class will no longer inherit the parent's __init__() function.
-"""
+# Inheritance with the child class overriding the parent's __init__()
 class Parent2():
     def __init__(self):
         print("Parent constructor invoked")
+
 class Child2(Parent2):
     def __init__(self):
         print("Child constructor invoked")
+
 obj2 = Child2()
+# Output: "Child constructor invoked"
+# Explanation: In this case, the child class (Child2) defines its own __init__() method,
+# which overrides the __init__() method of the parent class (Parent2). 
+# Therefore, when you create an instance of Child2, it calls the Child2 constructor.
 
-
-"""
-super().__init__() is used to call the constructor of the parent class 
-from within the constructor of the child class.
-"""
+# Using super() to call the parent class's __init__()
 class Parent3():
     def __init__(self):
         print("Parent constructor invoked")
+
 class Child3(Parent3):
     def __init__(self):
-        #Parent3.__init__(self)
-        super().__init__()
+        super().__init__()  # Using super() to call the parent's constructor
         print("Child constructor invoked")
+
 obj3 = Child3()
+# Output:
+# "Parent constructor invoked"
+# "Child constructor invoked"
+# Explanation: In this case, the child class (Child3) uses super().__init__() to explicitly call
+# the constructor of the parent class (Parent3). This allows the child class to inherit and
+# extend the behavior of the parent class's constructor.
+
+# Note: The commented out lines like "Parent3.__init__(self)" are an alternative way to call
+# the parent class's constructor, but using super() is considered a more Pythonic and flexible approach.
